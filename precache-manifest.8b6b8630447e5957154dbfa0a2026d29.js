e16721d829068f56aa906ee790241f781554a994
Evgenia-90 <qkj1111@gmail.com>
2021-04-04 19:17:57 +0100

    Updates

---
+++ precache-manifest.8b6b8630447e5957154dbfa0a2026d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d654bbc9df192b54c7eb23af9624bbd",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "608b7b4b17f0fc67642d",
+    "url": "/TodoList/static/css/main.75193674.chunk.css"
+  },
+  {
+    "revision": "68265c47e02ce4f41bff",
+    "url": "/TodoList/static/js/2.6aa6cd46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.6aa6cd46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608b7b4b17f0fc67642d",
+    "url": "/TodoList/static/js/main.15a29184.chunk.js"
+  },
+  {
+    "revision": "cec92663b5aa42fa8dea",
+    "url": "/TodoList/static/js/runtime-main.46cf6363.js"
+  }
+]);
